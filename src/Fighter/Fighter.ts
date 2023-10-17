e263a9d9a3be162f050b80860de0c3eb2d65d270
Douglas Araújo <devdoug45@gmail.com>
2023-10-17 07:52:43 -0300

    feat: req09

--- src/Fighter/Fighter.ts
+++ src/Fighter/Fighter.ts
@@ -1,14 +1,15 @@
 import Energy from '../Energy';
+import SimpleFighter from './SimpleFighter';
 
-export default interface Fighter {
+export default interface Fighter extends SimpleFighter {
   lifePoints: number;
   strength: number;
   defense: number;
   energy?: Energy;
 
-  attack(enemy: Fighter): void;
+  attack(enemy: Fighter | SimpleFighter): void;
 
-  special?(enemy: Fighter): void;
+  special?(enemy: Fighter | SimpleFighter): void;
 
   levelUp(): void;
 
---
+++ src/Monster.ts
@@ -0,0 +1,26 @@
+import { SimpleFighter } from './Fighter';
+
+export default class Monster implements SimpleFighter {
+  constructor(private _lifePoints = 85, private _strength = 63) {}
+
+  get lifePoints(): number {
+    return this._lifePoints;
+  }
+
+  get strength(): number {
+    return this._strength;
+  }
+
+  attack(enemy: SimpleFighter): void {
+    enemy.receiveDamage(this.strength);
+  }
+
+  receiveDamage(attackPoints: number): number {
+    this._lifePoints -= attackPoints;
+    if (this._lifePoints === 0) {
+      this._lifePoints = -1;
+    }
+
+    return this._lifePoints;
+  }
+}
